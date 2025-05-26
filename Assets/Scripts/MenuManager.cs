8e591109cfad97df90866cde07984d47997778f6
Adrian AB <jose.anchiraico@tecsup.edu.pe>
2025-05-25 19:27:59 -0500

    Game2 Cloud

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public TMP_Text nombreTexto;
+
+    void Awake()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        WebGLInput.captureAllKeyboardInput = false;
+#endif
+    }
+    public void ChangeScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void RecibirNombre(string nombre)
+    {
+        Debug.Log("Nombre recibido desde React: " + nombre);
+        nombreTexto.text = nombre; 
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            RecibirNombre("PruebaLocal");
+        }
+    }
+
+}
